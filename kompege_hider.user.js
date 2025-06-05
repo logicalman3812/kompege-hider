b18b843d547d652dd01ff91f930cd59c2b7589ac
logicalman3812 <85504304+logicalman3812@users.noreply.github.com>
2025-06-05 16:44:13 +0500

    initial commit

---
+++ kompege_hider.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         kompege | hide author's tasks
+// @namespace    https://github.com/logicalman3812/kompege-hider
+// @version      1.0
+// @description  for hide author's tasks
+// @match        https://kompege.ru/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/logicalman3812/kompege-hider/main/kompege-hider.user.js
+// @downloadURL  https://raw.githubusercontent.com/ТвойGitHubИмя/kompege-hide-authors/main/kompege-hide-authors.user.js
+// ==/UserScript==
